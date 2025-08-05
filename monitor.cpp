66dacc51eb0ed1779b4a022b08c5667aa2559908
Raghavendra S <sraghavendrasathish@gmail.com>
2025-08-05 08:42:56 +0530

    Update monitor.cpp

--- monitor.cpp
+++ monitor.cpp
@@ -1,38 +1,57 @@
 #include "./monitor.h"
-#include <assert.h>
+#include <iostream>
 #include <thread>
 #include <chrono>
-#include <iostream>
-using std::cout, std::flush, std::this_thread::sleep_for, std::chrono::seconds;
+using std::cout, std::flush;
+using std::this_thread::sleep_for;
+using std::chrono::seconds;
 
-int vitalsOk(float temperature, float pulseRate, float spo2) {
+// PURE FUNCTION: Checks whether vitals are OK
+enum VitalStatus {
+  VITAL_OK,
+  TEMP_CRITICAL,
+  PULSE_CRITICAL,
+  SPO2_CRITICAL
+};
+
+VitalStatus checkVitals(float temperature, float pulseRate, float spo2) {
   if (temperature > 102 || temperature < 95) {
-    cout << "Temperature is critical!\n";
-    for (int i = 0; i < 6; i++) {
-      cout << "\r* " << flush;
-      sleep_for(seconds(1));
-      cout << "\r *" << flush;
-      sleep_for(seconds(1));
-    }
-    return 0;
-  } else if (pulseRate < 60 || pulseRate > 100) {
-    cout << "Pulse Rate is out of range!\n";
-    for (int i = 0; i < 6; i++) {
-      cout << "\r* " << flush;
-      sleep_for(seconds(1));
-      cout << "\r *" << flush;
-      sleep_for(seconds(1));
-    }
-    return 0;
-  } else if (spo2 < 90) {
-    cout << "Oxygen Saturation out of range!\n";
-    for (int i = 0; i < 6; i++) {
-      cout << "\r* " << flush;
-      sleep_for(seconds(1));
-      cout << "\r *" << flush;
-      sleep_for(seconds(1));
-    }
-    return 0;
+    return TEMP_CRITICAL;
   }
-  return 1;
+  if (pulseRate < 60 || pulseRate > 100) {
+    return PULSE_CRITICAL;
+  }
+  if (spo2 < 90) {
+    return SPO2_CRITICAL;
+  }
+  return VITAL_OK;
+}
+
+// I/O FUNCTION: Alert animation
+void showAlert(const std::string& message) {
+  cout << message << '\n';
+  for (int i = 0; i < 6; i++) {
+    cout << "\r* " << flush;
+    sleep_for(seconds(1));
+    cout << "\r *" << flush;
+    sleep_for(seconds(1));
+  }
+}
+
+// MAIN FUNCTION: Orchestrates the above
+int vitalsOk(float temperature, float pulseRate, float spo2) {
+  VitalStatus status = checkVitals(temperature, pulseRate, spo2);
+  switch (status) {
+    case TEMP_CRITICAL:
+      showAlert("Temperature is critical!");
+      return 0;
+    case PULSE_CRITICAL:
+      showAlert("Pulse Rate is out of range!");
+      return 0;
+    case SPO2_CRITICAL:
+      showAlert("Oxygen Saturation out of range!");
+      return 0;
+    default:
+      return 1;
+  }
 }
